dc94dde08a8f3db2e7b9716913efdb34e616cb48
MarkoBitencCode <marko.bitenc@codelivery.team>
2024-05-22 07:50:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
+//state and effect
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    setCounter(100)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter} </h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+//////////////////////////////////////////////////////////////////////////
+//props
+/*
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello </h1>
+      <Person name={'John'} lastName={'Novak'} age={25} />
+      <Person name={'Edo'} lastName={'Jese'} age={35} />
+      <Person name={'Tilen'} lastName={'ReactJs'} age={23} />
+      <Person name={'Igor'} lastName={'Srdoc'} age={38} />
+    </div>
+  )
+}
+*/
+///////////////////////////////////////////////////////////////////////
+
+/*
+const App = () => {
+  const name = 'Marko'
+  const isNameVisible = false
+  return (
+    <div className="App">
+      <h1>Hello {isNameVisible ? name : 'amm ime ni vidno :( '}</h1>
+    </div>
+  )
+}
+ 
+*/
+
+//sample ternary expression
+/*
+const App = () => {
+  const name = 'Marko'
+
+  return (
+    <div className="App">
+      <h1>Hello </h1>
+      {name ? (
+        <>
+          <h1>{name}</h1>
+        </>
+      ) : (
+        <>
+          <h1>Test</h1>
+          <h2>There is no name</h2>
+        </>
+      )}
+    </div>
+  )
+}
+
+*/
+
+export default App
